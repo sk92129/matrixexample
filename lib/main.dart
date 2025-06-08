13a5d56b8ea8aa83663ee55539b038dc60caf0c9
Sean Kang <sk92129@gmail.com>
2025-06-07 22:58:04 -0700

    animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
-import 'package:flutter/material.dart';
-import 'dart:math' as math;
 
+import 'package:flutter/material.dart';
+import 'package:matrixexample/composed_button.dart';
+import 'package:matrixexample/rotated_animation.dart';
+import 'package:matrixexample/rotated_button.dart';
+import 'package:matrixexample/rotation_bounce_in_animation.dart';
+import 'package:matrixexample/rotation_builder_animation.dart';
+import 'package:matrixexample/scaled_button.dart';
+import 'package:matrixexample/translated_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,24 +15,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Animations',
       theme: ThemeData(
-
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Animations'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
 
   final String title;
 
@@ -35,60 +37,89 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
-
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Transform.rotate(
-              angle: -45 * (math.pi / 180.0),
-              child: ElevatedButton(
-                child: const Text('You have pushed the button this many times:'),
-                onPressed: () {},
-              ),
+            ElevatedButton(
+              child: Text('Rotated button'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    return RotatedButton();
+                  }),
+                );
+              },
             ),
-
-            Transform.scale(
-              scale: 2.0,
-              child: ElevatedButton(
-              child: Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                onPressed: () {},
-              ),
+            ElevatedButton(
+              child: Text('Scaled button'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    return ScaledButton();
+                  }),
+                );
+              },
             ),
-            
-            
+            ElevatedButton(
+              child: Text('Translated button'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    return TranslatedButton();
+                  }),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('Composed button'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    return ComposedButton();
+                  }),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('Animation button'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    return Scaffold(appBar: AppBar(), body: RotationAnimations());
+                  }),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('Animation bounce-in button'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    return Scaffold(appBar: AppBar(), body: RotationBounceInAnimations());
+                  }),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('Animation builder'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    return Scaffold(appBar: AppBar(), body: RotationBuilderAnimations());
+                  }),
+                );
+              },
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
